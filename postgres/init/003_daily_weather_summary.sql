ac9505e7e16f766a2f1f3337d864a23079d785d9
patril <patrick.rees.riley@gmail.com>
2026-08-26 16:22:58 -0400

    Track solar completeness in daily fingerprint

--- postgres/init/003_daily_weather_summary.sql
+++ postgres/init/003_daily_weather_summary.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS daily_weather_summary (
     mean_wind_mph DOUBLE PRECISION,
     peak_irradiance_w_m2 DOUBLE PRECISION,
     solar_energy_wh_m2 DOUBLE PRECISION,
+    solar_energy_is_complete BOOLEAN,
     mean_relative_pressure_inhg DOUBLE PRECISION,
     lightning_strikes INTEGER,
     sample_count INTEGER NOT NULL,
